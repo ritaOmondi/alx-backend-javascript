8110f998f89472f1d6787ae052407b3b46d2bb06
ritaOmondi <cherylritaomondi@gmail.com>
2024-07-31 09:25:23 +0000

    extending teachers interface on Director

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,10 @@ interface Teacher {
   [key: string]: any;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -15,4 +19,13 @@ const teacher3: Teacher = {
   contract: false,
 };
 
+const director1: Directors = {
+  firstName: 'Jane',
+  fullTimeEmployee: true,
+  lastName: 'Smith',
+  location: 'New York',
+  numberOfReports: 5,
+};
+
 console.log(teacher3);
+console.log(director1);

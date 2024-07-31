15d6c5f8a6182163283c7ff9968862ea2d7314d4
ritaOmondi <cherylritaomondi@gmail.com>
2024-07-31 09:41:35 +0000

    adding the student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,41 +1,32 @@
-interface Teacher {
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
   firstName: string;
   lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+  workOnHomework: () => string;
+  displayName: () => string;
 }
 
-interface Directors extends Teacher {
-  numberOfReports: number;
+class StudentClass implements StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
 
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
-
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-const director1: Directors = {
-  firstName: 'Jane',
-  fullTimeEmployee: true,
-  lastName: 'Smith',
-  location: 'New York',
-  numberOfReports: 5,
-};
-
-console.log(printTeacher('John', 'Doe')); // Output: J. Doe
-console.log(printTeacher('Jane', 'Smith')); // Output: J. Smith
-console.log(teacher3);
-console.log(director1);
+const student1: StudentClass = new StudentClass('John', 'Doe');
+console.log(student1.workOnHomework()); // Output: Currently working
+console.log(student1.displayName()); // Output: John

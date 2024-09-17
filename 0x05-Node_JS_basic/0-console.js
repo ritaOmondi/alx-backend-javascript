22844bfc32a329973431785f9da8ace38162d5f5
ritaOmondi <cherylritaomondi@gmail.com>
2024-09-17 12:22:35 +0300

    defining a function

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
-// 0-console.js
-const displayMessage = (message) => {
-    console.log(message);
-};
-module.exports = displayMessage;
+function displayMessage(string) {
+    console.log(string);
+  }
+  
+  module.exports = displayMessage;
